2b809676538adce7a9e2b715bc8a199172b5337b
Varunmnx <varun.cheriyath@gmail.com>
2025-02-25 10:55:44 +0530

    update test files and create palette

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,7 @@ export default defineConfig({
     globals: true, // Enables global `describe`, `test`, etc.
     environment: "jsdom", // Simulates a browser environment
     setupFiles: "./src/test/setup.ts", // Optional setup file
+    include: ["**/*.{test,spec}.{js,ts,jsx,tsx}"], // Custom include pattern
+    exclude: ["**/node_modules/**", "**/dist/**"], // Default exclude pattern
   },
 });
